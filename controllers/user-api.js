e2f23252925817368c155820f349ec20a4cd1783
Andreas Paljug <andreaspaljug@gmail.com>
2019-09-07 13:53:00 -0400

    added youtube classification attempt and api. need key from youtube dev account.

--- controllers/user-api.js
+++ controllers/user-api.js
@@ -6,5 +6,24 @@ router.post('/postHistory', (req, res) => {
     var response = "dummy response";
     res.json(response)
 })
+//fix this.
+router.post('/youtubeParse', (req, res) => { // I need youtube video id.
+    const id = '7lCDEYXw3mM&key'
+    var key = JSON.parse("/../keys.json");
+    const url =  "https://www.googleapis.com/youtube/v3/videos?id=" + id + "=" + key.youtubeAPI +
+     "&fields=items(id,snippet(channelId,title,categoryId),statistics)&part=snippet,statistics"
+
+
+    node-fetch(url)
+    .then(res => res.json())
+    .then((out) => {
+      console.log('Checkout this JSON! ', out);
+    })
+    .catch(err => { throw err });
+    console.log(req.body);
+    var response = "dummy response";
+    res.json(response)
+})
+
 
 module.exports = router;
