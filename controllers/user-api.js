d1bd0a17125e424879c4162e32bee0d835c22e7b
Marc D'Mello <marcd2000@gmail.com>
2019-09-07 15:10:33 -0400

    Created a map of host names to information

--- controllers/user-api.js
+++ controllers/user-api.js
@@ -1,8 +1,25 @@
+const url = require('url');
 const express = require('express');
 const router = express.Router();
 
 router.post('/postHistory', (req, res) => {
-    console.log(req.body);
+    var hostMap = new Map();
+    req.body.value.forEach(element => {
+        //console.log(element.url);
+        var myURL = new URL(element.url);
+        console.log(myURL.host);
+        if (!hostMap.get(myURL.host)) {
+            hostURLList = [];
+            hostURLList.push(element);
+            hostMap.set(myURL.host, hostURLList);
+        } else {
+            hostMap.get(myURL.host).push(element);
+        }
+    });
+    for (var key of hostMap.keys()) {
+        hostMap.get(key).forEach(element => console.log(element))
+    }
+    console.log(hostMap.keys());
     var response = "dummy response";
     res.json(response)
 })
